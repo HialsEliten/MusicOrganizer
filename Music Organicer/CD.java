b895e625da8ef978eef34512daa38554d2a5ba52
ks93 <kelvinsundli@hotmail.com>
2015-04-15 14:43:23 +0200

    CD, LP, Tape
    Added super to all, getters and setters to tape

--- Music Organicer/CD.java
+++ Music Organicer/CD.java
@@ -8,7 +8,10 @@
 public class CD extends Mediums
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private String artist;
+    private int releaseYear;
+    private String recordLabel;
+    private int archiveNumber;
 
     /**
      * Constructor for objects of class CD
@@ -17,7 +20,68 @@ public class CD extends Mediums
     {
         super(title);
         
+        
     }
+
+    /**
+     * Get artist name
+     */    
+    public String getArtist() {
+        return artist;
+    }
+    
+    /**
+     * Set new artist  
+     */ 
+    public void setArtist(String newArtist) {
+        this.artist = newArtist;
+    }
+    
+    /**
+     * Get release year  
+     */ 
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    
+    /**
+     * Set new release year  
+     */ 
+    public void setReleaseYear(int newReleaseYear) {
+        this.releaseYear = newReleaseYear;
+    }
+    
+    /**
+     * Get record label  
+     */ 
+    public String getRecordLabel() {
+        return recordLabel;
+    }
+    
+    /**
+     * Set new record label  
+     */ 
+    public void setRecordLabel(String newRecordLabel) {
+        this.recordLabel = newRecordLabel;
+    }
+    
+    /**
+     * Get archive number  
+     * 
+     * @return archiveNumber
+     */ 
+    public int getArchiveNumber() {
+        return archiveNumber;
+    }
+    
+    /**
+     * Set new archive number
+     * 
+     * @param newArchiveNumber
+     */ 
+    public void setArchiveNumber(int newArchiveNumber) {
+        this.archiveNumber = newArchiveNumber;
+    }  
 }
 
     
--- Music Organicer/LP.java
+++ Music Organicer/LP.java
@@ -13,21 +13,11 @@ public class LP extends Mediums
     /**
      * Constructor for objects of class LP
      */
-    public LP()
+    public LP(String title)
     {
-        // initialise instance variables
+        super(title);
         x = 0;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
