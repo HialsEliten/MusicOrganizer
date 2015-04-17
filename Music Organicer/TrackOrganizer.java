5300d5875ba51a7826a2f78d5c949fb33fed52d4
Vegard <Vegardrogne@hotmail.no>
2015-04-17 11:08:16 +0200

    TrackOrganizer
    Added track organizer and specified some fields

---
+++ Music Organicer/TrackOrganizer.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+/**
+ * Generating list of all exsiting tracks, regardless of its medium referance
+ * @author Vegard Rogne @ HialsEliten
+ */
+public class TrackOrganizer {
+    
+    // Initializing variables
+    ArrayList<AudioTrack> tracks; 
+    
+    // Constructor for TrackOrganizer
+    public TrackOrganizer()
+    {
+        // Assigning variables
+        tracks = new ArrayList<AudioTrack>();        
+    }
+
+        
+    /**
+     * Adds track to tacks
+     * 
+     * @param audioTrack audio track you want to add
+     */
+    public void addTrack(AudioTrack track) {
+        this.tracks.add(track);
+    }
+    
